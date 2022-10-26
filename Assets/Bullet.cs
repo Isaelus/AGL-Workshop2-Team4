6a65a6954f1cbc92ff2ab1572ed14f204f8c30d0
whatAreYouD0ingStepBr0 <nchong9158@sdsu.edu>
2022-10-26 11:40:12 -0700

    Player rotation, raygun and lazer prefab, bullet & shooting script
    also added 2 new enemies of different color for easy copy and paste
    i made a poor attempt at a green lazer explosion animation . (seeking help)
    **Bullets do not delete because there is no hitEffect gameObj assigned to it. needs work  **

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, transform.rotation);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+
+
+    }
+}
