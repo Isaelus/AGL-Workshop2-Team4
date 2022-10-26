9ace9bf9104008ff829b8228f97c882123852c65
Zayd <105383646+zaydjd@users.noreply.github.com>
2022-10-25 18:42:31 -0700

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,13 +10,17 @@ public class PlayerMovement : MonoBehaviour {
     public float dashCooldown = 0.5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
 
     Vector2 moveDir;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update() {
         moveDir.x = Input.GetAxisRaw("Horizontal");
         moveDir.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             canDash = true;
@@ -27,6 +31,10 @@ public class PlayerMovement : MonoBehaviour {
         rb.velocity = moveDir * MOVE_SPEED;
         if(canDash)
             StartCoroutine(Dash());
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) + Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 
     private IEnumerator Dash() {
