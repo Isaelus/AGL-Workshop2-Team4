4a3fee11cd08e3b3d90fcafa67deb486967add20
Ricardo Valdez <ricardovaldez125@gmail.com>
2022-10-27 23:49:44 -0700

    Display some text on screen
    shows how many tanks you have
    shows when you need wings when you try to board ship with just some gas.

--- Assets/Scripts/collectObjects.cs
+++ Assets/Scripts/collectObjects.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+using System;
+
 public class collectObjects : MonoBehaviour
 {
         public SpriteRenderer changesSprite;
@@ -14,12 +17,15 @@ public class collectObjects : MonoBehaviour
     private bool fullHealth;
     private float gasNeeded;
     private float health;
-
+    public TMP_Text gasText;
+    public TMP_Text youNeedWings;
 
     // Start is called before the first frame update
     void Start()
     { 
-        gasCollected = 0;
+         
+        gasCollected = 0;      
+        gasText.text = gasCollected.ToString() +"/5";
       
         wingCollected = false;
         fullGas = false;
@@ -38,17 +44,25 @@ public class collectObjects : MonoBehaviour
     {
         
     }
+    /*
+    *if statements when colliding with 
+    * FOOD, GAS , WINGS, & SHIP
+    */
     void OnTriggerEnter2D(Collider2D collectable){
+        //checks if gas is full if not then collect gas
        if (collectable.gameObject.CompareTag("Gas") && !fullGas ){
 
             Destroy(collectable.gameObject);
             gasCollected = gasCollected+1;
+             gasText.text = gasCollected.ToString()+"/5";
+        
             if(gasCollected == gasNeeded){
                     fullGas = true;
 
 
             }
        }
+       //checks if health is full if not then allowed to eat
        else if (collectable.gameObject.CompareTag("Food") && !fullHealth){
          
          Destroy(collectable.gameObject);
@@ -56,24 +70,41 @@ public class collectObjects : MonoBehaviour
 
 
        }
+       // collect the wing
        else if (collectable.gameObject.CompareTag("Wing")){
             Destroy(collectable.gameObject);
              wingCollected = true;
 
 
-        } else if (collectable.gameObject.CompareTag("Ship")){
+        } 
+        //GET IN SHIP 
+        else if (collectable.gameObject.CompareTag("Ship")){
                 if(wingCollected){
-
+                //Adds wings to ship
                         changesSprite.sprite = wingedShip;
+                }
+                if(fullGas && !wingCollected){
+                        //calls coroutine to remove text after x seconds
+                         StartCoroutine(popupText());
+                        
+                          
                 }
 
                 if(fullGas && wingCollected){
                         //GAME FINISHED
+                        //sprite changed to full
                          changesSprite.sprite = fullShip;
 
                 }
 
         }
+
+    }
+    //pops text waits 4 second and removes text
+    IEnumerator popupText(){
+         youNeedWings.text = "you need WINGS to fly";
+        yield return new WaitForSeconds(4f);
+         youNeedWings.text = "";
 
     }
 }
