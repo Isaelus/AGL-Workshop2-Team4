eb0fcb690c0cf61ada6627afa120c7c9549b3280
Zayd <zdixon008@gmail.com>
2022-10-27 17:48:18 -0700

    Revert "Dashing to smooth movement instead of blink"
    This reverts commit 96cc2fa6aafe675aa42832a6823969226166e9ab.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public const float MOVE_SPEED = 5f;
-    bool canDash = true;
-    bool canMove = true;
-    public float dashSpeed = 10f;
-    public float dashTime = 0.25f;
-    public float dashCooldown = 1f;
-    float currentDashTime;
-    float currentCooldown;
+    bool canDash;
+    public float dashDist = 3f;
+    public float dashTime = 1f;
+    public float dashCooldown = 0.5f;
 
     public Rigidbody2D rb;
     public Camera cam;
@@ -23,47 +20,29 @@ public class PlayerMovement : MonoBehaviour {
         moveDir.x = Input.GetAxisRaw("Horizontal");
         moveDir.y = Input.GetAxisRaw("Vertical");
 
-        // mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetKeyDown(KeyCode.Space) && canDash) {
-            StartCoroutine(Dash(moveDir));
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            canDash = true;
         }
     }
 
     void FixedUpdate() {
-        if (canMove) 
-            rb.velocity = moveDir * MOVE_SPEED;
+        rb.velocity = moveDir * MOVE_SPEED;
+        if(canDash)
+            StartCoroutine(Dash());
 
-        // Vector2 lookDir = mousePos - rb.position;
-        // float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        // rb.rotation = angle;
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 
-    private IEnumerator Dash(Vector2 direction) {
+    private IEnumerator Dash() {
         canDash = false;
-        canMove = false;
-        currentDashTime = dashTime;
-        while (currentDashTime > 0f) {
-            currentDashTime -= Time.deltaTime;
-            
-            if (direction.x != 0 && direction.y != 0) {
-                float diagSpeed = (dashSpeed / 2) + 1;
-                rb.velocity = direction * diagSpeed;
-            } else {
-                rb.velocity = direction * dashSpeed;
-            }
-                
-            yield return null;
-        }
-        
-        rb.velocity = new Vector2(0f, 0f);
-        
-        currentCooldown = dashCooldown;
-        while (currentCooldown > 0f) {
-            currentCooldown -= Time.deltaTime;
-            canMove = true;
-            yield return null;
-        }
-        canDash = true;
+        bool isDashing = true;
+        rb.MovePosition((Vector2)transform.position + moveDir * dashDist);
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
     }
 }
